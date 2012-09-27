6c31c3ffe15195e7a6518916b356734ed70c7394
Jacques <jba@zenexity.com>
2012-09-27 14:57:58 +0200

    Add Org joda time

--- framework/src/anorm/src/main/scala/anorm/Anorm.scala
+++ framework/src/anorm/src/main/scala/anorm/Anorm.scala
@@ -143,6 +143,24 @@ object Column {
     }
   }
 
+  implicit def rowToDateTime: Column[org.joda.time.DateTime] = Column.nonNull { (value, meta) =>
+    import org.joda.time.DateTime
+    val MetaDataItem(qualified, nullable, clazz) = meta
+    value match {
+      case d: java.sql.Date => Right( new DateTime( d.getTime ) )
+      case t: java.sql.Timestamp => Right( new DateTime( t.getTime ) )
+      case _ => Left(TypeDoesNotMatch("Cannot convert " + value + ":" + value.asInstanceOf[AnyRef].getClass + " to DateTime for column " + qualified) )
+    }
+  }
+
+  implicit def rowToDateMidnight: Column[org.joda.time.DateMidnight] = Column.nonNull { (value, meta) =>
+    val MetaDataItem(qualified, nullable, clazz) = meta
+    value match {
+      case d: java.sql.Date => Right( new org.joda.time.DateMidnight( d.getTime ) )
+      case _ => Left(TypeDoesNotMatch("Cannot convert " + value + ":" + value.asInstanceOf[AnyRef].getClass + " to DateMidnight for column " + qualified) )
+    }
+  }
+
   implicit def rowToPk[T](implicit c: Column[T]): Column[Pk[T]] = Column.nonNull { (value, meta) =>
     c(value, meta).map(Id(_))
 
@@ -338,7 +356,12 @@ object ToStatement {
 
   implicit val dateToStatement = new ToStatement[java.util.Date] {
     def set(s: java.sql.PreparedStatement, index: Int, aValue: java.util.Date): Unit = s.setTimestamp(index, new java.sql.Timestamp(aValue.getTime()))
+  }
 
+  implicit def readableInstantToStatement[A <: org.joda.time.ReadableInstant] = new ToStatement[A] {
+    def set(s: java.sql.PreparedStatement, index: Int, aValue: A): Unit = {
+      s.setTimestamp(index, new java.sql.Timestamp(aValue.getMillis()) )
+    }
   }
 
   implicit def optionToStatement[A](implicit ts: ToStatement[A]): ToStatement[Option[A]] = new ToStatement[Option[A]] {
